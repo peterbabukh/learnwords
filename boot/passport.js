505247dcf4ffdc831bbe8e9ddb4d63b9534d74b8
peterbabukh <peter_babukh@mail.ru>
2016-01-07 23:51:23 +0400

    Google reCaptcha done

--- boot/passport.js
+++ boot/passport.js
@@ -1,6 +1,4 @@
-var crypto = require('crypto');
 var async = require('async');
-var util = require('util');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var flash = require('connect-flash');
@@ -9,6 +7,7 @@ var User = require('../models/User').User;
 var Words = require('../Words');
 var WordSchema = require('../models/WordSchema');
 var i18n = require('i18next');
+var googleReCaptcha = require('../lib/googleReCaptcha');
 
 module.exports = function (app) {
 
@@ -28,34 +27,50 @@ module.exports = function (app) {
         },
         function(req, username, password, done) {
 
-            // check in mongo if a user with username exists or not
-            User.findOne({ 'username':  username },
-                function(err, user) {
-                    // In case of any error, return using the done method
-                    if (err)
-                        return done(err);
-                    // Username does not exist, log error & redirect back
-                    if (!user){
-                        console.log('User Not Found with username ' + username);
-                        return done(null, false,
-                            req.flash('message', i18n.t('text.invalidEmailOrPassword') ) );
-                    }
 
-                    // User exists but wrong password, log the error
-                    // it uses User.schema.checkPassword method
-                    if (!user.checkPassword(password)){
-                        console.log('Invalid Password');
+            // checks googleReCaptcha
+            googleReCaptcha(req.body["g-recaptcha-response"], function(success) {
+                if (!success) {
 
-                        return done(null, false,
-                            req.flash('message', i18n.t('text.invalidEmailOrPassword') ) );
-                    }
+                    console.log('Invalid recaptcha');
+                    return done(null, false,
+                        req.flash('message', i18n.t('text.invalidRecaptcha') ) );
 
-                    // User and password both match, return user from
-                    // done method which will be treated like success
-                    req.session.user = user;
-                    return done(null, user);
+                } else {
+
+                    // checks in mongodb if a user with username exists or not
+                    User.findOne({ 'username':  username },
+                        function(err, user) {
+                            // In case of any error, return using the done method
+                            if (err)
+                                return done(err);
+                            // Username does not exist, log error & redirect back
+                            if (!user){
+                                console.log('User Not Found with username ' + username);
+                                return done(null, false,
+                                    req.flash('message', i18n.t('text.invalidEmailOrPassword') ) );
+                            }
+
+                            // User exists but wrong password, log the error
+                            // it uses User.schema.checkPassword method
+                            if (!user.checkPassword(password)){
+                                console.log('Invalid Password');
+
+                                return done(null, false,
+                                    req.flash('message', i18n.t('text.invalidEmailOrPassword') ) );
+                            }
+
+                            // User and password both match, return user from
+                            // done method which will be treated like success
+                            req.session.user = user;
+                            return done(null, user);
+                        }
+                    );
+
                 }
-            );
+            });
+
+
         }
     ));
 
@@ -65,63 +80,76 @@ module.exports = function (app) {
         },
         function(req, username, password, done) {
 
-            findOrCreateUser = function() {
+            // checks googleReCaptcha
+            googleReCaptcha(req.body["g-recaptcha-response"], function (success) {
+                // if recaptcha fails...
+                if (!success) {
 
-                // find a user in Mongo with provided username
-                User.findOne({'username': username}, function(err, user) {
-                    // In case of any error return
-                    if (err){
-                        console.log('Error in SignUp: ' + err);
-                        return done(err);
-                    }
-                    // already exists
-                    if (user) {
-                        console.log('User already exists');
-                        return done(null, false,
-                            req.flash('message', i18n.t('text.suchUserExists') ) );
-                    } else {
-                        // if there is no user with that email
-                        // create the user
-                        var newUser = new User();
+                    console.log('Invalid recaptcha');
+                    return done(null, false,
+                        req.flash('message', i18n.t('text.invalidRecaptcha')));
 
-                        // set the user's local credentials
-                        // it is registration by email and password, NOT by name
-                        //so here username is email
-                        newUser.username = username;
-                        newUser.salt = Math.random() + '';
-                        newUser.hashedPassword = newUser.encryptPassword(password);
-                        newUser.words = [];
-                        newUser.selectOptions = [];
+                } else {
 
-                        // create and save word models and push them to user
-                        _.each(Words, function(item) {
-                            item._user_id = newUser._id;
-                            var word = new WordSchema(item);
-                            word.save(function(err){
-                                if (err) return console.log(err);
-                            });
-                            newUser.words.push( word );
-                        });
+                    findOrCreateUser = function () {
 
-                        // save the user
-                        newUser.save(function(err) {
-                            if (err){
-                                console.log('Error in Saving user: ' + err);
-                                throw err;
+                        // find a user in Mongo with provided username
+                        User.findOne({'username': username}, function (err, user) {
+                            // In case of any error return
+                            if (err) {
+                                console.log('Error in SignUp: ' + err);
+                                return done(err);
                             }
-                            // set a cookie with the newUser's info
-                            req.session.user = newUser;
-                            console.log('User Registration succesful');
-                            return done(null, newUser);
+                            // already exists
+                            if (user) {
+                                console.log('User already exists');
+                                return done(null, false,
+                                    req.flash('message', i18n.t('text.suchUserExists')));
+                            } else {
+                                // if there is no user with that email
+                                // create the user
+                                var newUser = new User();
+
+                                // set the user's local credentials
+                                // it is registration by email and password, NOT by name
+                                //so here username is email
+                                newUser.username = username;
+                                newUser.salt = Math.random() + '';
+                                newUser.hashedPassword = newUser.encryptPassword(password);
+                                newUser.words = [];
+                                newUser.selectOptions = [];
+
+                                // create and save word models and push them to user
+                                _.each(Words, function (item) {
+                                    item._user_id = newUser._id;
+                                    var word = new WordSchema(item);
+                                    word.save(function (err) {
+                                        if (err) return console.log(err);
+                                    });
+                                    newUser.words.push(word);
+                                });
+
+                                // save the user
+                                newUser.save(function (err) {
+                                    if (err) {
+                                        console.log('Error in Saving user: ' + err);
+                                        throw err;
+                                    }
+                                    // set a cookie with the newUser's info
+                                    req.session.user = newUser;
+                                    console.log('User Registration succesful');
+                                    return done(null, newUser);
+                                });
+
+                            }
                         });
+                    };
 
-                    }
-                });
-            };
-
-            // Delay the execution of findOrCreateUser and execute
-            // the method in the next tick of the event loop
-            process.nextTick(findOrCreateUser);
+                    // Delay the execution of findOrCreateUser and execute
+                    // the method in the next tick of the event loop
+                    process.nextTick(findOrCreateUser);
+                }
+            });
         }
     ));
 
