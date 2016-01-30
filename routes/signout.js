bfd5f7b75fef7fff5b8bda1e674c03322c4b14d9
peterbabukh <peter_babukh@mail.ru>
2016-01-30 11:04:54 +0400

    words delete upon signout fix

--- routes/signout.js
+++ routes/signout.js
@@ -2,14 +2,30 @@ var User = require('../models/User').User;
 
 exports.get = function(req, res) {
 
-    User.remove({'_id': req.session.user._id}, function (err) {
+    // remove all words attached to this user
+    User.findOne({'_id': req.session.user._id}, function(err, user) {
         if (err) return console.log(err);
 
-        req.session.destroy(function(err) {
+        // deletes all words
+        while (user.words.length) {
+            user.words[0].remove();
+        }
+
+        // saves the user to fix changes
+        user.save(function(err) {
+            if (err) return console.log(err);
+        });
+
+        // remove the user
+        User.remove({'_id': req.session.user._id}, function (err) {
             if (err) return console.log(err);
 
-            return res.redirect('/');
+            req.session.destroy(function(err) {
+                if (err) return console.log(err);
 
+                return res.redirect('/');
+
+            });
         });
     });
 };
